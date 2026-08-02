36f7d52979c8d5a809aea7f52cf83e52ab85f05e
Real World Impact <sackman.louis@gmail.com>
2026-08-01 23:22:29 -0500

    Add files via upload

---
+++ profile-registry-config.js
@@ -0,0 +1,11 @@
+window.RWI_PROFILE_REGISTRY = Object.freeze({
+  "chainId": 4663,
+  "address": "0x09bF3DE73BFf7bc43EE6799909ab29eeBFE9EF9f",
+  "deploymentBlock": 25606405,
+  "deploymentTransaction": "0x69f945a9156eb33a944acee7811cf8f8425b0a5564dc59b141f76dc67a5bd3bb",
+  "sourceVerified": true,
+  "independentAuditComplete": false,
+  "codeVersion": 1,
+  "runtimeCodeHash": "0x073dc3e49afdbc3abe63ad699c89d501cb56e9250c015984d09834fe6dc33e64",
+  "validatedAtBlock": 25608766
+});
